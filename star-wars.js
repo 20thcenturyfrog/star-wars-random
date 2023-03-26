2526704c98fbad2f73eac4e484acfcc4d3947f4f
20thcenturyfrog <pogorelova.katerina@yandex.ru>
2023-03-26 22:07:45 +0300

    First commit

---
+++ star-wars.js
@@ -0,0 +1,75 @@
+const form = document.forms[0];
+const resourceSelector = document.getElementById("resource");
+const numberInput = document.getElementById("number");
+const button = document.getElementById("button");
+const containerDiv = document.getElementById("containerDiv");
+const errorDiv = document.getElementById("errorDiv");
+const loader = document.getElementById("loading");
+const options = {
+  people: "Человек",
+  planets: "Планета",
+  species: "Существо",
+  starships: "Космический корабль",
+  vehicles: "Другой вид транспорта",
+};
+
+document.addEventListener("DOMContentLoaded", () =>
+  setDropdownOptions(options)
+);
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  containerDiv.innerHTML = "";
+  errorDiv.innerHTML = "";
+
+  displayLoading();
+
+  const requestData = {
+    resource: resourceSelector.options[resourceSelector.selectedIndex].value,
+    number: numberInput.value,
+  };
+
+  form.reset();
+
+  fetch(`https://swapi.dev/api/${requestData.resource}/${requestData.number}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then((item) => {
+      hideLoading();
+      const foundItem = document.createElement("p");
+      foundItem.className = "found-item";
+      foundItem.innerHTML = `${options[requestData.resource]} под номером ${
+        requestData.number
+      } — это ${item.name}.`;
+      containerDiv.appendChild(foundItem);
+    })
+    .catch((error) => {
+      hideLoading();
+      const errorMessage = document.createElement("p");
+      errorMessage.className = "error-message";
+      errorMessage.innerHTML = `<p class="error-message-text">Извините, мы ничего не нашли! Код ошибки: ${error.message}.</p><p class="error-message-text">Попробуйте ещё раз!</p>`;
+      errorDiv.appendChild(errorMessage);
+    });
+});
+
+function setDropdownOptions(x) {
+  for (let i in x) {
+    const opt = document.createElement("option");
+    opt.value = i;
+    opt.innerHTML = x[i];
+    resourceSelector.appendChild(opt);
+  }
+}
+
+function displayLoading() {
+  loader.classList.add("display");
+}
+
+function hideLoading() {
+  loader.classList.remove("display");
+}
